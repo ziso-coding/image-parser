d6bd916286d3c38ab5277ea2d20c4ccc4643d72d
shevelev <andrei.zisoshewelew@mail.ru>
2021-07-17 13:44:58 +0300

    resource for readme

---
+++ resources/output.kt
@@ -0,0 +1,13 @@
+private fun getMultipleArray(): List<List<Byte>> {
+    return listOf(
+        listOf(0, 1, 0, 0, 0, 0, 0, 0, 1, 0),
+        listOf(0, 1, 1, 1, 1, 1, 1, 1, 1, 0),
+        listOf(0, 0, 0, 0, 1, 1, 0, 0, 0, 0),
+        listOf(1, 1, 0, 0, 1, 1, 0, 0, 1, 1),
+        listOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+        listOf(1, 1, 0, 0, 1, 1, 0, 0, 1, 1),
+        listOf(1, 0, 0, 0, 1, 1, 0, 0, 0, 1),
+        listOf(0, 0, 1, 1, 1, 1, 1, 1, 0, 0),
+        listOf(0, 0, 1, 0, 1, 1, 0, 1, 0, 0),
+        listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    )
